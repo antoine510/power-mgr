9cd79774e1c19827e7acf56f9c6c84ff8be8a383
Antoine Richard <antoine_510@hotmail.fr>
2025-01-19 09:50:37 +0100

    Now using distant InfluxDB server, quitting on successive failures

--- src/power-mgr.cpp
+++ src/power-mgr.cpp
@@ -5,10 +5,12 @@
 #include <influxdb.hpp>
 
 static constexpr const char* house_device = "/dev/power-house";
-static constexpr const char* edf_solar_device = "/dev/power-solar";
+static constexpr const char* solar_device = "/dev/power-solar";
 
-static constexpr const char* influxdb_org_name = "PowerPi";
-static constexpr const char* influxdb_house_bucket = "HousePower";
+static constexpr const char* influxdb_ip = "192.168.1.37";
+static constexpr const int influxdb_port = 8086;
+static constexpr const char* influxdb_org_name = "Microtonome";
+static constexpr const char* influxdb_bucket = "EDF";
 
 using LogPeriod = std::chrono::minutes;
 using ExtraLogPeriod = std::chrono::hours;
@@ -16,6 +18,7 @@ using ExtraLogPeriod = std::chrono::hours;
 static constexpr const std::chrono::seconds sample_interval = std::chrono::seconds(5);
 static constexpr const auto sample_count = LogPeriod(1) / sample_interval;
 
+static constexpr unsigned maxReadFails = 4;
 
 PowerData houseData[sample_count];
 PowerData averageSamples(PowerData* dataArray) {
@@ -35,7 +38,7 @@ PowerData averageSamples(PowerData* dataArray) {
 }
 
 int main(int argc, char** argv) {
-	std::unique_ptr<PowerMeter> houseMeter, edfSolarMeter;
+	std::unique_ptr<PowerMeter> houseMeter, solarMeter;
 	std::string influxdb_token;
 	try {
 		auto env_token = getenv("INFLUXDB_TOKEN");
@@ -43,21 +46,21 @@ int main(int argc, char** argv) {
 		influxdb_token = env_token;
 
 		houseMeter = std::make_unique<PowerMeter>(house_device);
-		edfSolarMeter = std::make_unique<PowerMeter>(edf_solar_device);
+		solarMeter = std::make_unique<PowerMeter>(solar_device);
 	} catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return -1;
 	}
 
-	influxdb_cpp::server_info serverInfo("127.0.0.1", 8086, influxdb_org_name, influxdb_token, influxdb_house_bucket);
+	influxdb_cpp::server_info serverInfo(influxdb_ip, influxdb_port, influxdb_org_name, influxdb_token, influxdb_bucket);
 
 	influxdb_cpp::builder()
-		.meas("HouseExtra")
+		.meas("Extra")
 		.field("restart", true)
 		.post_http(serverInfo);
 
 	auto nextExtraTP = std::chrono::ceil<ExtraLogPeriod>(std::chrono::system_clock::now());
-
+	unsigned readFailCount = 0;
 	while(true) {
 		const auto currentTP = std::chrono::system_clock::now();
 		const auto nextMinuteTP = std::chrono::ceil<LogPeriod>(currentTP);
@@ -71,7 +74,7 @@ int main(int argc, char** argv) {
 			}
 
 			PowerData houseAverage = averageSamples(houseData);
-			PowerData edfSolarData = edfSolarMeter->ReadAll();
+			PowerData solarData = solarMeter->ReadAll();
 
 			influxdb_cpp::builder()
 				.meas("House")
@@ -82,21 +85,23 @@ int main(int argc, char** argv) {
 				.post_http(serverInfo);
 
 			influxdb_cpp::builder()
-				.meas("EDFSolar")
-				.field("current", edfSolarData.current_ma / 1000.f, 3)
-				.field("power", edfSolarData.power_dw / 10.f, 1)
-				.field("cos_phi", edfSolarData.power_factor / 100.f, 2)
+				.meas("Solar")
+				.field("current", solarData.current_ma / 1000.f, 3)
+				.field("power", solarData.power_dw / 10.f, 1)
+				.field("cos_phi", solarData.power_factor / 100.f, 2)
 				.post_http(serverInfo);
 
 			if(currentTP > nextExtraTP) {
 				influxdb_cpp::builder()
-					.meas("HouseExtra")
-					.field("energy", houseAverage.energy_wh / 1000.f, 3)
-					.field("edf_solar_energy", edfSolarData.energy_wh / 1000.f, 3)
+					.meas("Extra")
+					.field("house_e", houseAverage.energy_wh / 1000.f, 3)
+					.field("solar_e", solarData.energy_wh / 1000.f, 3)
 					.post_http(serverInfo);
 				nextExtraTP = std::chrono::ceil<ExtraLogPeriod>(currentTP);
 			}
+			readFailCount = 0;
 		} catch(const std::exception& e) {
+			if(++readFailCount > maxReadFails) return -1;
 			std::cerr << e.what() << std::endl;
 		}
 	}
