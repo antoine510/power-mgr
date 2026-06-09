4e8d7557a705c3d05dd1212acf6ce8e192289e20
Antoine RICHARD <antoine_510@hotmail.fr>
2026-06-09 21:04:08 +0200

    Improved error logging

--- src/Marstek.cpp
+++ src/Marstek.cpp
@@ -59,14 +59,14 @@ VenusE::UsefulInfo VenusE::GetUsefulInfo(int id) {
 nlohmann::json VenusE::SendRequest(nlohmann::json&& req) {
 	req["id"] = callID++;
 	std::string outBuf = req.dump();
-	if(send(_fd, outBuf.data(), outBuf.size(), 0) <= 0) throw std::runtime_error("Error sending data to battery");
+	if(send(_fd, outBuf.data(), outBuf.size(), 0) <= 0) throw std::runtime_error("Error sending data to battery: " + std::to_string(errno));
 	auto deadline = std::chrono::steady_clock::now() + std::chrono::milliseconds(500);
 	int numRead = 0;
 	while(true) {
 		numRead = recv(_fd, _recvBuffer, sizeof(_recvBuffer), MSG_DONTWAIT);
 		if(numRead > 0) break;
 		if(errno == EAGAIN) {
-			if(std::chrono::steady_clock::now() > deadline) throw std::runtime_error("Timed out");
+			if(std::chrono::steady_clock::now() > deadline) throw TimeoutError();
 			std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		} else {
 			throw std::runtime_error("Error receiving data from battery: " + std::to_string(errno));
